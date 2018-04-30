166a988b1e5981c95179c88b971f1dabaccc11f5
ahmedyehya92 <ahmedyehya1992@gmail.com>
2018-04-30 16:58:38 +0200

    new

--- app/src/main/java/com/ibtikartechs/apps/el7a2/ui/activities/main/MainActivity.java
+++ app/src/main/java/com/ibtikartechs/apps/el7a2/ui/activities/main/MainActivity.java
@@ -1,15 +1,34 @@
 package com.ibtikartechs.apps.el7a2.ui.activities.main;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.ibtikartechs.apps.el7a2.R;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.main_toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+    }
+
+    public void setupActionBar() {
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
     }
 }
