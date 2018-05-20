07850c298b8026f9634451d5b22a9dfaf4d4a6bb
ahmedyehya92 <ahmedyehya1992@gmail.com>
2018-05-20 09:41:20 +0200

    new

---
+++ app/src/main/java/com/ibtikartechs/apps/el7a2/data/adapters/PlaceAutoCompleteAdapter.java
@@ -0,0 +1,246 @@
+package com.ibtikartechs.apps.el7a2.data.adapters;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.constraint.ConstraintLayout;
+import android.text.style.CharacterStyle;
+import android.text.style.StyleSpan;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.data.DataBufferUtils;
+import com.google.android.gms.location.places.AutocompleteFilter;
+import com.google.android.gms.location.places.AutocompletePrediction;
+import com.google.android.gms.location.places.AutocompletePredictionBufferResponse;
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.tasks.RuntimeExecutionException;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.ibtikartechs.apps.el7a2.R;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by ahmedyehya on 5/19/18.
+ */
+
+public class PlaceAutoCompleteAdapter extends ArrayAdapter<AutocompletePrediction> implements Filterable {
+    private static final String TAG = "PlaceAutocomplete";
+    private static final CharacterStyle STYLE_BOLD = new StyleSpan(Typeface.BOLD);
+    ViewHolder viewHolder;
+
+    private ArrayList<AutocompletePrediction> mResultList;
+    private OnAutoLocationItemClickListner onAutoLocationItemClickListner;
+
+    /**
+     * Handles autocomplete requests.
+     */
+    private GeoDataClient mGeoDataClient;
+
+    /**
+     * The bounds used for Places Geo Data autocomplete API requests.
+     */
+    private LatLngBounds mBounds;
+
+    /**
+     * The autocomplete filter used to restrict queries to a specific set of place types.
+     */
+    private AutocompleteFilter mPlaceFilter;
+
+    /**
+     * Initializes with a resource for text rows and autocomplete query bounds.
+     *
+     * @see android.widget.ArrayAdapter#ArrayAdapter(android.content.Context, int)
+     */
+
+    public PlaceAutoCompleteAdapter(Context context, GeoDataClient geoDataClient,
+                                    LatLngBounds bounds, AutocompleteFilter filter) {
+        super(context, android.R.layout.simple_expandable_list_item_2, android.R.id.text1);
+        mGeoDataClient = geoDataClient;
+        mBounds = bounds;
+        mPlaceFilter = filter;
+
+    }
+
+    public void setBounds(LatLngBounds bounds) {
+        mBounds = bounds;
+    }
+
+    @Override
+    public int getCount() {
+        return mResultList.size();
+    }
+
+    @Override
+    public AutocompletePrediction getItem(int position) {
+        return mResultList.get(position);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        final AutocompletePrediction item = getItem(position);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.view_place_autocomplete_row, parent, false);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        }
+        else
+            viewHolder = (ViewHolder) convertView.getTag();
+
+        viewHolder.tvMainTitle.setText(item.getPrimaryText(STYLE_BOLD));
+        viewHolder.tvSubTitle.setText(item.getSecondaryText(STYLE_BOLD));
+        viewHolder.loutItemContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onAutoLocationItemClickListner.onAutoLocationItemClicked(item.getPlaceId());
+            }
+        });
+
+        return convertView;
+    }
+
+
+    public class ViewHolder {
+        @BindView(R.id.textView)
+        TextView tvMainTitle;
+        @BindView(R.id.textView2)
+        TextView tvSubTitle;
+        @BindView(R.id.lout_container)
+        ConstraintLayout loutItemContainer;
+
+        public ViewHolder(View itemView) {
+            ButterKnife.bind(this, itemView);
+        }
+    }
+
+
+    public interface OnAutoLocationItemClickListner {
+
+        public void onAutoLocationItemClicked(String placeId);
+    }
+
+    public void setOnAutoLocationItemClickListner(OnAutoLocationItemClickListner onAutoLocationItemClickListner)
+    {
+        this.onAutoLocationItemClickListner = onAutoLocationItemClickListner;
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults results = new FilterResults();
+
+                // We need a separate list to store the results, since
+                // this is run asynchronously.
+                ArrayList<AutocompletePrediction> filterData = new ArrayList<>();
+
+                // Skip the autocomplete query if no constraints are given.
+                if (constraint != null) {
+                    // Query the autocomplete API for the (constraint) search string.
+                    filterData = getAutocomplete(constraint);
+                }
+
+                results.values = filterData;
+                if (filterData != null) {
+                    results.count = filterData.size();
+                } else {
+                    results.count = 0;
+                }
+
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+
+                if (results != null && results.count > 0) {
+                    // The API returned at least one result, update the data.
+                    mResultList = (ArrayList<AutocompletePrediction>) results.values;
+
+                    notifyDataSetChanged();
+                } else {
+                    // The API did not return any results, invalidate the data set.
+                    notifyDataSetInvalidated();
+                }
+            }
+
+            @Override
+            public CharSequence convertResultToString(Object resultValue) {
+                // Override this method to display a readable result in the AutocompleteTextView
+                // when clicked.
+                if (resultValue instanceof AutocompletePrediction) {
+                    return ((AutocompletePrediction) resultValue).getFullText(null);
+                } else {
+                    return super.convertResultToString(resultValue);
+                }
+            }
+        };
+    }
+
+    /**
+     * Submits an autocomplete query to the Places Geo Data Autocomplete API.
+     * Results are returned as frozen AutocompletePrediction objects, ready to be cached.
+     * Returns an empty list if no results were found.
+     * Returns null if the API client is not available or the query did not complete
+     * successfully.
+     * This method MUST be called off the main UI thread, as it will block until data is returned
+     * from the API, which may include a network request.
+     *
+     * @param constraint Autocomplete query string
+     * @return Results from the autocomplete API or null if the query was not successful.
+     * @see GeoDataClient#getAutocompletePredictions(String, LatLngBounds, AutocompleteFilter)
+     * @see AutocompletePrediction#freeze()
+     */
+    @SuppressLint("RestrictedApi")
+    private ArrayList<AutocompletePrediction> getAutocomplete(CharSequence constraint) {
+        Log.i(TAG, "Starting autocomplete query for: " + constraint);
+
+        // Submit the query to the autocomplete API and retrieve a PendingResult that will
+        // contain the results when the query completes.
+        Task<AutocompletePredictionBufferResponse> results =
+                mGeoDataClient.getAutocompletePredictions(constraint.toString(), mBounds,
+                        mPlaceFilter);
+
+        // This method should have been called off the main UI thread. Block and wait for at most
+        // 60s for a result from the API.
+        try {
+            Tasks.await(results, 60, TimeUnit.SECONDS);
+        } catch (ExecutionException | InterruptedException | TimeoutException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            AutocompletePredictionBufferResponse autocompletePredictions = results.getResult();
+
+            Log.i(TAG, "Query completed. Received " + autocompletePredictions.getCount()
+                    + " predictions.");
+
+            // Freeze the results immutable representation that can be stored safely.
+            return DataBufferUtils.freezeAndClose(autocompletePredictions);
+        } catch (RuntimeExecutionException e) {
+            // If the query did not complete successfully return null
+            Toast.makeText(getContext(), "Error contacting API: " + e.toString(),
+                    Toast.LENGTH_SHORT).show();
+            Log.e(TAG, "Error getting autocomplete prediction API call", e);
+            return null;
+        }
+    }
+
+}
